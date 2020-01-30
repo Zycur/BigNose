63e92732b11bb16ff8ef534ee06e03381eea2ddf
Zycur <likeaboss_69@hotmail.com>
2020-01-30 21:41:27 +0000

    added game

---
+++ Big Nose/Assets/BulletFly.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFly : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    Collider2D hitInfo;
+    public int damage = 25;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if(hitInfo.name == "Main Bird")
+        {
+            Health hp = (Health) hitInfo.gameObject.GetComponent(typeof(Health));
+            hp.TakeDamage(damage);
+        }
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Big Nose/Assets/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health = health - damage;
+        Debug.Log(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
